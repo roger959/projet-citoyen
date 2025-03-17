9bbb27a27001e7fbb01403a0de2084f6bc20b83a
roger959 <r.lumbala@ecole-ipssi.net>
2025-03-17 12:41:41 +0100

    ameliorations +++

--- index.js
+++ index.js
@@ -42,4 +42,12 @@ function handleClick(page) {
         } else {
             gallery.classList.add('hidden');
         }
+    }   
+    function toggleMenu() {
+        const sideMenu = document.getElementById('sideMenu');
+        if (sideMenu.style.left === '0px') {
+            sideMenu.style.left = '-250px';
+        } else {
+            sideMenu.style.left = '0px';
+        }
     }
